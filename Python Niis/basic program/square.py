2dffedf74739bfe75ee6eddc575104f2c9537a81
Ankita <ankitapriyadarsinisahu19@gmail.com>
2026-02-23 10:41:12 +0530

    basic program

---
+++ Python Niis/basic program/square.py
@@ -0,0 +1,8 @@
+"""wap take square length and breadth from keyboard display length breadth area and perimeter"""
+
+print("enter square side=")
+s=int(input())
+ar=s*s
+pr=4*s
+print("area=",ar)
+print("perimeter=",pr)
---
+++ Python Niis/basic program/sum of 2 digits no.py
@@ -0,0 +1,5 @@
+"""Wap to find sum of digits of a 2-digit number."""
+print("enter 2 digit number")
+n=int(input())
+s=(n//10)+(n%10)
+print("sum=",s)
