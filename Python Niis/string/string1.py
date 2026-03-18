f6774be1f8db6ef996123a95c54b92488661e311
Ankita <ankitapriyadarsinisahu19@gmail.com>
2026-03-18 10:32:24 +0530

    string

---
+++ Python Niis/string/string1.py
@@ -0,0 +1,29 @@
+#display individual letter line by line
+s = "welcome"
+for i in range(0,7,1):
+	print(s[i])
+
+#display individual letter line by line
+s = "welcome ram"
+for i in range(0,len(s),1):
+	print(s[i])
+
+
+#display individual letter line by line
+s = "welcome"
+for i in range(-len(s),0,1):
+	print(s[i])
+
+
+
+
+#display individual letter backward
+s = "welcome"
+for i in range(len(s)-1,-1,-1):
+	print(s[i])
+
+
+
+s = "welcome"
+for i in range(-1,len(s)-1,-1):
+	print(s[i])
---
+++ Python Niis/string/string2.py
@@ -0,0 +1,12 @@
+#display individual letter in forward using squence
+s = "welcome"
+for i in s:
+	print(i)
+
+
+
+#display individual letter in backward using squence
+s = "welcome"
+s = s[::-1]
+for i in s:
+	print(i)
